bf3f76cacd58e255ad44162884eaa05c5ccc2239
CesarGorge <cesargorgeee@gmail.com>
2022-10-14 19:29:28 +0000

    Excuse Generator listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuseGenerator(who, action, what, when) {
+  numWho = Math.floor(Math.random() * who.length);
+  numAction = Math.floor(Math.random() * action.length);
+  numWhat = Math.floor(Math.random() * what.length);
+  numWhen = Math.floor(Math.random() * when.length);
+  return (
+    who[numWho] +
+    " " +
+    action[numAction] +
+    " " +
+    what[numWhat] +
+    " " +
+    when[numWhen]
+  );
+}
+function onLoad() {
+  var excuse = document.getElementById("excuse");
+  excuse.innerHTML = excuseGenerator(who, action, what, when);
+}
+window.onload = onLoad;
